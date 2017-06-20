79197876748759c46352e5949e4edceda7e9ed90
Markus Kont <markuskont@gmail.com>
2017-06-20 14:32:51 +0300

    Initial commit

---
+++ bloom.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "math"
+)
+
+// n = number of elements in data set
+// p = acceptable false positive 0 < p < 1 (no checks atm)
+// m = estimated size of bloom filter array
+// m = -1 * float64(n) * math.Log(p) / math.Pow(math.Log(2), 2)
+// k = num of needed hash functions
+// hash = hashing method to use ( <= 1 for murmur, 2 for fnv, else mix of both)
+type BloomFilter struct {
+  m     uint
+  k     uint
+  bits  []bool
+  hash  int
+}
+
+func NewBloomWithEstimate(n uint, p float64, h int) (b *BloomFilter, err error) {
+  m, k := estimate(n, p)
+  b = &BloomFilter{
+    m:    m,
+    k:    k,
+    hash: h,
+  }
+  b.bits = make([]bool, m)
+  return b, err
+}
+
+func estimate(n uint, p float64) (m, k uint) {
+  //m = math.Ceil(( float64(n) * math.Log(p) ) / math.Log(1.0 / math.Pow( 2.0, math.Log(2.0) )))
+  size := math.Ceil(-1 * float64(n) * math.Log(p) / math.Pow( math.Log(2.0), 2.0 ))
+  k = uint( round(math.Log(2.0) * size / float64(n)) )
+  m = uint( size )
+  // max size for 32bit integer
+  if m > 4294967295 { log.Fatal("Estimated bitarray length ", m, " does not fit in unsigned 32bit integer. Dataset size is ", n, " and confidence is ", p, ". Try lowering.") }
+  return
+}
+
+func round(f float64) float64 {
+  return math.Floor(f + .5)
+}
+
+func genLocs(data []byte, b *BloomFilter) (locations []uint64) {
+  locations = make([]uint64, b.k)
+  h := genHashBase(data, b)
+  for i := uint64(0); i < uint64(b.k); i++ {
+    locations[i] = transformHashes(h[0], h[1], i) % uint64(b.m)
+  }
+  return
+}
+
+func (b *BloomFilter) Add(data []byte) *BloomFilter {
+  locations := genLocs(data, b)
+  for i := range locations {
+    if b.bits[locations[i]] == true { fmt.Println("Collision!") }
+    b.bits[locations[i]] = true
+  }
+  return b
+}
+
+func (b *BloomFilter) AddString(data string) *BloomFilter {
+  return b.Add([]byte(data))
+}
+
+func (b *BloomFilter) Query(data []byte) bool {
+  locations := genLocs(data, b)
+  for i := range locations {
+    if b.bits[locations[i]] == false {
+      // one missing bit is enough to verify non-existence, exit ASAP
+      return false
+    }
+  }
+  return true
+}
+
+func (b *BloomFilter) QueryString(data string) bool {
+  return b.Query([]byte(data))
+}
---
+++ hasher.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+  "hash/fnv"
+
+  "github.com/spaolacci/murmur3"
+)
+
+// generate base hash values which will be later used for uniques hashes
+func genHashBase(data []byte, b *BloomFilter) (h [2]uint64) {
+  switch {
+  case b.hash <= 1:
+    h = genHashBaseMurmur(data)
+  case b.hash == 2:
+    h = genHashBaseFnv(data)
+  case b.hash >= 3:
+    h = genHashBaseCombo(data)
+  }
+  return
+}
+
+// this function uses murmur3 hash
+// 128bit integer split into 2 distinct 64bit sections
+func genHashBaseMurmur(data []byte) [2]uint64 {
+  hasher := murmur3.New128()
+  hasher.Write(data)
+  h1, h2 := hasher.Sum128()
+  return [2]uint64{
+    h1, h2,
+  }
+}
+// simple fnv
+func genHashBaseFnv(data []byte) [2]uint64 {
+  hasher := fnv.New64a()
+  hasher.Write(data)
+  h1 := hasher.Sum64()
+  hasher.Write([]byte{1})
+  h2 := hasher.Sum64()
+  return [2]uint64{
+    h1, h2,
+  }
+}
+// mix of fnv and 64bit murmur3
+func genHashBaseCombo(data []byte) [2]uint64 {
+  hasher1 := fnv.New64a()
+  hasher1.Write(data)
+  h1 := hasher1.Sum64()
+  hasher2 := murmur3.New64()
+  hasher2.Write(data)
+  h2 := hasher2.Sum64()
+  return [2]uint64{
+    h1, h2,
+  }
+}
+
+// https://www.eecs.harvard.edu/~michaelm/postscripts/rsa2008.pdf
+func transformHashes(h1, h2, i uint64) uint64 {
+  return h1 + i * h2
+}
